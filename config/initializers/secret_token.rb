b3dc80776ba76bbfbf0caa9013218dec443acc36
Brian Williams <brian_williams1994@yahoo.com>
2012-07-09 14:33:10 -0500

    start rails project Brian

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CPS::Application.config.secret_token = 'f121dffdc0b15e1e9818ab993c424773df2bd401fad5834cabecb6b4dfb1650ba031805668949d41056fd0864c260c765fdf3b4071b8dcbd512cec5beb79d520'
