16bffbaebd52de6fda1ac930d8590245a0c2b989
Ryan Oglesby <ryan.oglesby08@gmail.com>
2012-07-18 14:03:09 -0500

    Brian/Ryan Deleted null and NDA from dataset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,23 +4,26 @@ class ApplicationController < ActionController::Base
   def graphdata
     render "graph"
   end
-  
-  def fake_data
-     #[{x:1, y:2},{schoolName:"Chicago Tech", x:3,y:5}]
-  end
 
   def data
     raw_cps_data = File.open("#{Rails.root}/vendor/assets/javascripts/schools.json", 'r').read
     needed_cps_data = JSON.parse(raw_cps_data)["data"]
-  
-    safety_score_and_parental_involvement = needed_cps_data.map{|school|
-      "{safetyScore:#{school[25]}, parentInvolvement: #{school[27]}}"
-  }
+
+    safety_score_and_parental_involvement = needed_cps_data.select { |school|
+      safetyScore = school[25]
+      parentInvolvement = school[27]
+      safetyScore != "NDA" and parentInvolvement != "NDA"
+    }
 
 
-  #Extract school name, parental involvement score, safety score
-  #Return info through render
-  render :json=>safety_score_and_parental_involvement
-end
+    safety_score_and_parental_involvement.map! { |school|
+      safetyScore = school[25]
+      parentInvolvement = school[27]
+
+      "{safetyScore:#{safetyScore}, parentInvolvement: #{parentInvolvement}, schoolName: '#{school[9]}'}"
+    }
+
+    render :json => safety_score_and_parental_involvement
+  end
 
 end
