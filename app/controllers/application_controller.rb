b471cb9565de327712d3ed0c2a99499516304625
Ryan Oglesby <ryan.oglesby08@gmail.com>
2012-07-18 14:35:43 -0500

    Brian/Ryan added httpparty and connected the data from website

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,15 +6,14 @@ class ApplicationController < ActionController::Base
   end
 
   def data
-    raw_cps_data = File.open("#{Rails.root}/vendor/assets/javascripts/schools.json", 'r').read
-    needed_cps_data = JSON.parse(raw_cps_data)["data"]
+
+    needed_cps_data = get_data_from_file()
 
     safety_score_and_parental_involvement = needed_cps_data.select { |school|
       safetyScore = school[25]
       parentInvolvement = school[27]
       safetyScore != "NDA" and parentInvolvement != "NDA"
     }
-
 
     safety_score_and_parental_involvement.map! { |school|
       safetyScore = school[25]
@@ -24,6 +23,15 @@ class ApplicationController < ActionController::Base
     }
 
     render :json => safety_score_and_parental_involvement
+  end
+
+  def get_data_from_file
+    raw_cps_data = File.open("#{Rails.root}/vendor/assets/javascripts/schools.json", 'r').read
+    JSON.parse(raw_cps_data)["data"]
+  end
+
+  def get_data_from_website
+    HTTParty.get "https://data.cityofchicago.org/api/views/9xs2-f89t/rows.json"
   end
 
 end
